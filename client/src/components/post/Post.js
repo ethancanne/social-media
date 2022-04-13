de72162107b396d2ae7e7eeef68f57625bbef113
Conor Helmick <chelmick1@liberty.edu>
2022-04-13 09:31:33 -0400

    Error resolution for Post interface functionality

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import React, { useState } from "react";
 
 const Post = ({ post }) => {
     return (
         <div>
             <div className="postTitle">Title: {post.title}</div>
             <img className="postImage" src={post.image} alt />
+            <div className="postBody">{post.body}</div>
             <button className="supports">Supports {post.supports}</button>
             <button className="opposes">Opposes {post.opposes}</button>
 
