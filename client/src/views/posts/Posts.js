f9e6ac7389630b63c24ce3a8ed109cf3e6a9edf5
Conor Helmick <chelmick1@liberty.edu>
2022-04-20 09:17:28 -0400

    Using mui for post card

--- client/src/views/posts/Posts.js
+++ client/src/views/posts/Posts.js
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 const Posts = ({ }) => {
 
 
-    const posts = [{ title: "Title", body: "Some text", supports: 44, opposes: 12 }]
+    const posts = [{ title: "Title", timestamp: Date.now(), body: "Some text", supports: 44, opposes: 12 }, { title: "Title", timestamp: Date.now(), body: "Some text", supports: 44, opposes: 12 }]
     return (
         <div className="PostsContainer">
             {posts.map(post => (
