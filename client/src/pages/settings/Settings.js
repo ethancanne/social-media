279ffe8f16e1ad7708b2e74fd259cc5a141b487f
Ethan Cannelongo <ethancannelongo@Ethans-MacBook-Pro.local>
2022-04-22 16:42:25 -0400

    Started settings page and removed mongo from docker compose

--- client/src/pages/settings/Settings.js
+++ client/src/pages/settings/Settings.js
@@ -1,12 +1,52 @@
 import "./Settings.scss";
 import React from "react";
 import { Page, pages } from "../Page";
+import ProfileIcon from "@mui/icons-material/AccountBox";
+import LockIcon from "@mui/icons-material/Lock";
+import ThemeIcon from "@mui/icons-material/DarkMode";
+import ArrowIcon from "@mui/icons-material/ArrowForwardIos";
 
 const Settings = () => {
   return (
     <Page currentPage={pages.SETTINGS}>
       <div className='settings-page'>
-        <h1>Settings</h1>
+        <div className='settings-page-header'>
+          <h1>Settings</h1>
+          <p>General</p>
+        </div>
+
+        <div className='settings-list-container'>
+          <div className='settings-list-item'>
+            <div className='settings-item-left'>
+              <ProfileIcon />
+              <h1>Edit Profile</h1>
+              <p>Edit you information available to anyone and everyone</p>
+            </div>
+            <div className='settings-item-right'>
+              <ArrowIcon />
+            </div>
+          </div>
+          <div className='settings-list-item'>
+            <div className='settings-item-left'>
+              <LockIcon />
+              <h1>Change Password</h1>
+              <p>Did you forget... Again</p>
+            </div>
+            <div className='settings-item-right'>
+              <ArrowIcon />
+            </div>
+          </div>
+          <div className='settings-list-item'>
+            <div className='settings-item-left'>
+              <ThemeIcon />
+              <h1>Theme</h1>
+              <p>Is this application hard on your eyes? Try dark mode!</p>
+            </div>
+            <div className='settings-item-right'>
+              <button>Change</button>
+            </div>
+          </div>
+        </div>
       </div>
     </Page>
   );
